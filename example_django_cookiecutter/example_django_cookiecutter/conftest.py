a04b4c2c5ef81e4cb123149d1d40a29ff936391d
Anne <morakot@unixdev.co.th>
2024-07-15 21:08:44 +0700

    feat: init

---
+++ example_django_cookiecutter/example_django_cookiecutter/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from example_django_cookiecutter.users.models import User
+from example_django_cookiecutter.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
